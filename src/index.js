8ef4e124ba4bcc8e1044a54fede13db48c640a5e
Sebastian Estrada <sbstnmz@gmail.com>
2024-01-02 17:15:07 -0300

    :hammer: fix jobs to scrape (with company name included)

--- src/index.js
+++ src/index.js
@@ -11,27 +11,15 @@ async function scrapeJobs(url) {
     const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
-    const featuredJobs = $('.gb-featured-job');
     const jobs = $('.sgb-results-list > div');
-    const totalJobs = featuredJobs.length + jobs.length;
-
-    featuredJobs.each((i, el) => {
-      const company = $(el).find('strong').text().trim();
-      const title = $(el).find('h4').text().trim();
-
-      console.log(`Título: ${title}`);
-      console.log(`Empresa: ${company}`);
-      console.log('-----------------------');
-    });
+    const totalJobs = jobs.length;
 
     jobs.each((i, el) => {
-      const title = $(el).find('strong').text().trim();
+      const title = $(el).find('.gb-results-list__title strong').text().trim();
       const company = $(el)
-        .find('.gb-results-list__info .size0')
-        .first()
-        .contents()
-        .get(0)
-        .nodeValue.trim();
+        .find('.gb-results-list__info .size0 strong')
+        .text()
+        .trim();
 
       console.log(`Título: ${title}`);
       console.log(`Empresa: ${company}`);
